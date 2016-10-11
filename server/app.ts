c591b6750a72645a977e5f57a82d6de189a17d03
Oded Dwek <oded.walla@gmail.com>
2016-10-11 22:43:04 +0300

    Initial commit

---
+++ server/app.ts
@@ -0,0 +1,42 @@
+import * as express from "express";
+import { join } from "path";
+import * as favicon from "serve-favicon";
+import { json, urlencoded } from "body-parser";
+
+class Server {
+    app: any;
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    static bootstrap() {
+        return new Server();
+    }
+
+    config() {
+        this.app.set("views", join(__dirname, "views"));
+        this.app.set("view engine", "jade");
+        this.app.use(json());
+        this.app.use(urlencoded({ extended: true }));
+        this.app.use(express.static(join(__dirname, "public")));
+        this.app.use("/client", express.static(join(__dirname, "client")));
+        this.app.use((err, req, res, next) => {
+            var error = new Error("Not Found");
+            err.status = 404;
+            next(err);
+        });
+    }
+
+    routes() {
+        // let router;
+        // router = express.Router();
+        // var index = new indexRoute.Index();
+        // router.get("/", index.index.bind(index.index));
+        // this.app.use(router);
+    }
+}
+
+var server = Server.bootstrap();
+module.exports = server.app;
