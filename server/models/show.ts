5546f668d8d213b737228a3f1dbbc17b8caffebe
Oded Dwek <oded.walla@gmail.com>
2016-10-24 19:14:06 +0300

    Server: Removed name from the model, TvMaze Id is all we need.

--- server/models/show.ts
+++ server/models/show.ts
@@ -2,12 +2,10 @@ import mongoose = require("mongoose");
 
 export interface IShow extends mongoose.Document {
     tvMazeId: number;
-    name: string;
 };
 
-export const ShowSchema : mongoose.Schema = new mongoose.Schema({
-    name: { type: String, required: true },
-    tvMazeId: Number,
+export const ShowSchema: mongoose.Schema = new mongoose.Schema({
+    tvMazeId: { type: Number, required: true },
 });
 
 export const ShowModel = mongoose.model<IShow>("Show", ShowSchema);
